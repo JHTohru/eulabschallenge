715283cb7cc71b3b5a3bd3b6f9195b6d8a237abe
tohru <6404916+JHTohru@users.noreply.github.com>
2024-05-17 20:18:39 -0300

    adds custom http error handler

---
+++ pkg/echohandler/error-handler.go
@@ -0,0 +1,24 @@
+package echohandler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	"github.com/JHTohru/eulabschallenge/pkg/product"
+)
+
+func httpErrorHandler(err error, c echo.Context) {
+	switch {
+	case errors.As(err, new(*malformationError)):
+		c.String(http.StatusBadRequest, err.Error())
+	case errors.As(err, new(*product.ValidationError)):
+		c.String(http.StatusUnprocessableEntity, err.Error())
+	case errors.Is(err, product.ErrNotFound):
+		c.String(http.StatusNotFound, err.Error())
+	default:
+		c.Logger().Error(err)
+		c.String(http.StatusInternalServerError, "internal error")
+	}
+}
---
+++ pkg/echohandler/error-handler_test.go
@@ -0,0 +1,63 @@
+package echohandler
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo"
+
+	"github.com/JHTohru/eulabschallenge/pkg/product"
+)
+
+func TestHttpErrorHandler(t *testing.T) {
+	tests := map[string]struct {
+		err        error
+		statusWant int
+		bodyWant   string
+	}{
+		"it must respond with the bad request status for a malformation request error": {
+			err:        newMalformationError("fake malformation error"),
+			statusWant: http.StatusBadRequest,
+			bodyWant:   "fake malformation error",
+		},
+		"it must respond with the unprocessable entity status for validation errors": {
+			err:        product.NewValidationError("fake validation error"),
+			statusWant: http.StatusUnprocessableEntity,
+			bodyWant:   "fake validation error",
+		},
+		"it must respond with the not found status for validation errors": {
+			err:        product.ErrNotFound,
+			statusWant: http.StatusNotFound,
+			bodyWant:   "product not found",
+		},
+		"it must respond with the internal server error status for an unexpected errors": {
+			err:        fmt.Errorf("fake error"),
+			statusWant: http.StatusInternalServerError,
+			bodyWant:   "internal error",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			resp := httptest.NewRecorder()
+			c := e.NewContext(req, resp)
+
+			httpErrorHandler(test.err, c)
+
+			respBody := resp.Body.String()
+			respBody = strings.TrimSuffix(respBody, "\n")
+			if respBody != test.bodyWant {
+				t.Errorf("unexpected response body; got %q, want %q", respBody, test.bodyWant)
+			}
+
+			if resp.Code != test.statusWant {
+				t.Errorf("unexpected response status code; got %d, want %d", resp.Code, test.statusWant)
+			}
+		})
+	}
+}
